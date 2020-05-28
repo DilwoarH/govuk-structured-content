714fce1ddbcd92f38629445c511d7303eba14402
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2020-05-28 11:07:46 +0100

    add netlify function

--- src/structured-content.js
+++ src/structured-content.js
@@ -1,16 +1,29 @@
+'use strict';
+
 const html2json = require('html2json').html2json;
 const fetch = require('node-fetch');
 
 let url = "https://www.gov.uk/api/content/guidance/software-developer";
 
-fetch(url)
+exports.handler = function(event, context, callback) {
+  console.log('DATE! ' + (new Date()));
+  getFiles().then(res => {
+    
+  }).catch(e => {
+    callback(e);
+  });
+  fetch(url)
   .then(res => res.json())
   .then(content_item => {
     let html = content_item.details.body.replace(/(\r\n|\n|\r)/gm, "");
     let object = html2json(html);
     let json = JSON.stringify(object, null, 2);
-    console.log(json);
+    callback(null, {
+      statusCode: 200,
+      body: json
+    });
+  })
+  .catch(e => {
+    callback(e);
   });
-
-
-
+}
