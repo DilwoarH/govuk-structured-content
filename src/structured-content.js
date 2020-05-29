d4f833995aba7780935c49c9e98e94c3ea93f0ed
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2020-05-29 14:22:26 +0100

    fixes errors

--- src/structured-content.js
+++ src/structured-content.js
@@ -1,15 +1,18 @@
 'use strict';
 require('encoding');
 const html2json = require('html2json').html2json;
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 let url = "https://www.gov.uk/api/content/guidance/software-developer";
 
 exports.handler = function(event, context, callback) {
   console.log('DATE! ' + (new Date()));
-  fetch(url)
-  .then(res => res.json())
-  .then(content_item => {
+  console.log('URL! ' + url);
+
+  axios
+  .get(url)
+  .then(result => {
+    let content_item = result.data;
     let html = content_item.details.body.replace(/(\r\n|\n|\r)/gm, "");
     let object = html2json(html);
     let json = JSON.stringify(object, null, 2);
